e6a2f06841adf4f41af988c4214c7c5e14765e37
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-06-22 18:29:57 +0200

    start 15

---
+++ 15_1_Data.py
@@ -0,0 +1,12 @@
+###########################################
+# Book "Python Crash course" - Eric Matthes
+# Chapter 15: Generating Data
+###########################################
+
+###########################################
+# simple line graph using matplotlib
+
+import matplotlib.pyplot as plt
+squares = [1,4,9,16,25]
+plt.plot(squares)
+plt.show()
